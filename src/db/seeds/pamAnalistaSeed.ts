6f6a72f16580581700907bc9419430b74900ccc2
Jhonatan Vargas <jvargas@sefin.gob.hn>
2026-04-13 14:01:44 -0600

    fix: actualizando librerias y seed de analistas

--- src/db/seeds/pamAnalistaSeed.ts
+++ src/db/seeds/pamAnalistaSeed.ts
@@ -8,177 +8,178 @@ export type NuevoAnalista = InferInsertModel<typeof PamAnalista>;
 
 export async function seedPamAnalistas() {
   const analistas: NuevoAnalista[] = [
-  {
-    "id": 1,
-    "nombre": "DAVID ZELAYA",
-    "usuario": "dzelaya",
-    "password": "$2b$10$gFsJzBvDn/9GVKYal6xnXOMgf6jKG/sLuQZR282KXHAozdylmq2VS",
-    "modulo": "E",
-    "isJefe": "N",
-    "oficina": "TGU",
-    "isActivo": "S"
-  },
-  {
-    "id": 2,
-    "nombre": "JARRIET AMADOR",
-    "usuario": "jamador",
-    "password": "$2b$10$S5YBDyc1keZ4nCwsI1KvTO.726kQ4iDPtf89BhD4GU6tlNVYBUM0O",
-    "modulo": "E",
-    "isJefe": "N",
-    "oficina": "TGU",
-    "isActivo": "S"
-  },
-  {
-    "id": 3,
-    "nombre": "IRIS GONZALEZ",
-    "usuario": "igonzalez",
-    "password": "$2b$10$.Et2cvD5Re28hO/wvFNHaOYNRD08nr76xXhWYGgsieCByGH841sO2",
-    "modulo": "E",
-    "isJefe": "S",
-    "oficina": "TGU",
-    "isActivo": "S"
-  },
-  {
-    "id": 4,
-    "nombre": "NANCY ACEITUNO",
-    "usuario": "naceituno",
-    "password": "$2b$10$VOKbnP1nGmI7BfTp1x31xezAbIOYaxk4teQplsLUhWA3rb1KG3E6q",
-    "modulo": "E",
-    "isJefe": "N",
-    "oficina": "TGU",
-    "isActivo": "S"
-  },
-  {
-    "id": 5,
-    "nombre": "DEBORA RAMOS",
-    "usuario": "deramos",
-    "password": "$2b$10$3CwQ.7FbZkeRV4aIOsI3I.1IhQIDAZJISPlYV58407oJvzDZOwTCu",
-    "modulo": "E",
-    "isJefe": "N",
-    "oficina": "TGU",
-    "isActivo": "N"
-  },
-  {
-    "id": 6,
-    "nombre": "SUAM LI",
-    "usuario": "sclopez",
-    "password": "$2b$10$qOEh60mC9.GfMSFFwlkIS.IRVFkf71zdpEZf5gBS6gcH.DZuWGdqC",
-    "modulo": "D",
-    "isJefe": "S",
-    "oficina": "TGU",
-    "isActivo": "S"
-  },
-  {
-    "id": 7,
-    "nombre": "LILI BADOS",
-    "usuario": "lbados",
-    "password": "$2b$10$v5BG4alpSvnMq.DgOOpjY.czhB63Znvi94R/ZONa5gJUyJwGY77PS",
-    "modulo": "D",
-    "isJefe": "N",
-    "oficina": "TGU",
-    "isActivo": "S"
-  },
-  {
-    "id": 8,
-    "nombre": "MARIAM GOMEZ",
-    "usuario": "mgomez",
-    "password": "$2b$10$JfaZVfQZ4507xj7JBJuJnOM5fBLTTMyWSbndHiSOAnqKnlVDtS7sy",
-    "modulo": "D",
-    "isJefe": "N",
-    "oficina": "TGU",
-    "isActivo": "S"
-  },
-  {
-    "id": 9,
-    "nombre": "MANUEL ORTEGA",
-    "usuario": "mortega",
-    "password": "$2b$10$ZKUUI0ABy47zNPXsj7EraetDhGvBVvPEiwnNBu4xxuJM/25moPFvW",
-    "modulo": "D",
-    "isJefe": "N",
-    "oficina": "TGU",
-    "isActivo": "S"
-  },
-  {
-    "id": 10,
-    "nombre": "EVA VALERIANO",
-    "usuario": "evaleriano",
-    "password": "$2b$10$frx9B1.Mo6..nqtkUJJukOOLkvv/MX0Qw25f1JsyPCR4x0zl28BMq",
-    "modulo": "D",
-    "isJefe": "N",
-    "oficina": "TGU",
-    "isActivo": "S"
-  },
-  {
-    "id": 11,
-    "nombre": "JOSE LARA",
-    "usuario": "jlara",
-    "password": "$2b$10$P0ueMC7bDxN9Vt8kq4DkVeVjLuxPe7pwUbaaobed/TA5O1zGiQYZS",
-    "modulo": "E",
-    "isJefe": "N",
-    "oficina": "SPS",
-    "isActivo": "S"
-  },
-  {
-    "id": 12,
-    "nombre": "ANA FLORES",
-    "usuario": "aflores",
-    "password": "$2b$10$o7zJDJOIpmYiNeWTOSmIveNSmCI5.SkLVqYheP6ip00ZFks2XAEYe",
-    "modulo": "E",
-    "isJefe": "S",
-    "oficina": "SPS",
-    "isActivo": "S"
-  },
-  {
-    "id": 13,
-    "nombre": "JOSUE PINEDA",
-    "usuario": "jpineda",
-    "password": "$2b$10$BmCIhY3ApUzjMfLSsmOz2.GtwmDoUJjnA7mcyiy.0cdOZ01vy77q.",
-    "modulo": "E",
-    "isJefe": "N",
-    "oficina": "SPS",
-    "isActivo": "S"
-  },
-  {
-    "id": 14,
-    "nombre": "KAREN MILLA",
-    "usuario": "kmilla",
-    "password": "$2b$10$Mm/WWENNS1nOp0WTx37yTublIQJQTV99IDWjhhni35/1ps96RQt7e",
-    "modulo": "E",
-    "isJefe": "N",
-    "oficina": "SPS",
-    "isActivo": "S"
-  },
-  {
-    "id": 15,
-    "nombre": "SARA PADILLA",
-    "usuario": "spadilla",
-    "password": "$2b$10$910x4TTr2V1oicxKZrp0c.bMyFlGZlQ7zw.R6kk9jpG/uyhO8Ch4m",
-    "modulo": "D",
-    "isJefe": "S",
-    "oficina": "SPS",
-    "isActivo": "S"
-  },
-  {
-    "id": 16,
-    "nombre": "ANA LEIVA",
-    "usuario": "aleiva",
-    "password": "$2b$10$3uEMSxT//r56GiSUXgA7CubsUMASqp0veJ3qrNJu2T7dOPPMDXvMW",
-    "modulo": "D",
-    "isJefe": "N",
-    "oficina": "SPS",
-    "isActivo": "S"
-  },
-  {
-    "id": 17,
-    "nombre": "ANYI MARTINEZ",
-    "usuario": "anmartinez",
-    "password": "$2b$10$gNi7Ek22oYqKkKz4PAFjKu/d4XnB8zoRfwYwosC4wFBeVOM37lGcq",
-    "modulo": "E",
-    "isJefe": "N",
-    "oficina": "TGU",
-    "isActivo": "S"
-  }
-]
+    //EXONERACIONES - TGU
+    {
+      nombre: "DAVID ZELAYA",
+      usuario: "dzelaya",
+      password: bcrypt.hashSync("0801198209948", 10),
+      modulo: "E",
+      isJefe: "N",
+      oficina: "TGU",
+      isActivo: "S",
+    },
+    {
+      nombre: "JARRIET AMADOR",
+      usuario: "jamador",
+      password: bcrypt.hashSync("0801198123205", 10),
+      modulo: "E",
+      isJefe: "N",
+      oficina: "TGU",
+      isActivo: "S",
+    },
+    {
+      nombre: "IRIS GONZALEZ",
+      usuario: "igonzalez",
+      password: bcrypt.hashSync("0801198014231", 10),
+      modulo: "E",
+      isJefe: "S",
+      oficina: "TGU",
+      isActivo: "S",
+    },
+    {
+      nombre: "NANCY ACEITUNO",
+      usuario: "naceituno",
+      password: bcrypt.hashSync("0801198300685", 10),
+      modulo: "E",
+      isJefe: "N",
+      oficina: "TGU",
+      isActivo: "S",
+    },
+    {
+      nombre: "DEBORA RAMOS",
+      usuario: "deramos",
+      password: bcrypt.hashSync("0318199801258", 10),
+      modulo: "E",
+      isJefe: "N",
+      oficina: "TGU",
+      isActivo: "N",
+    },
+    {
+      nombre: "Anyi Martinez".toUpperCase(),
+      usuario: "anmartinez",
+      password: bcrypt.hashSync("0801199820049", 10),
+      modulo: "E",
+      isJefe: "N",
+      oficina: "TGU",
+      isActivo: "S",
+    },
+
+    // DISPENSAS - TGU
+    {
+      nombre: "SUAM LI",
+      usuario: "sclopez",
+      password: bcrypt.hashSync("0801199720689", 10),
+      modulo: "D",
+      isJefe: "S",
+      oficina: "TGU",
+      isActivo: "S",
+    },
+    {
+      nombre: "LILI BADOS",
+      usuario: "lbados",
+      password: bcrypt.hashSync("0801198126295", 10),
+      modulo: "D",
+      isJefe: "N",
+      oficina: "TGU",
+      isActivo: "S",
+    },
+    {
+      nombre: "MARIAM GOMEZ",
+      usuario: "mgomez",
+      password: bcrypt.hashSync("0801199418846", 10),
+      modulo: "D",
+      isJefe: "N",
+      oficina: "TGU",
+      isActivo: "S",
+    },
+    {
+      nombre: "MANUEL ORTEGA",
+      usuario: "mortega",
+      password: bcrypt.hashSync("0506197300394", 10),
+      modulo: "D",
+      isJefe: "N",
+      oficina: "TGU",
+      isActivo: "S",
+    },
+    {
+      nombre: "EVA VALERIANO",
+      usuario: "evaleriano",
+      password: bcrypt.hashSync("0801200211583", 10),
+      modulo: "D",
+      isJefe: "N",
+      oficina: "TGU",
+    },
+
+    // EXONERACIONES - SPS
+    {
+      nombre: "JOSE LARA",
+      usuario: "jlara",
+      password: bcrypt.hashSync("1623198100766", 10),
+      modulo: "E",
+      isJefe: "N",
+      oficina: "SPS",
+      isActivo: "S",
+    },
+
+    {
+      nombre: "ANA FLORES",
+      usuario: "aflores",
+      password: bcrypt.hashSync("0501199804729", 10),
+      modulo: "E",
+      isJefe: "S",
+      oficina: "SPS",
+      isActivo: "S",
+    },
+    {
+      nombre: "JOSUE PINEDA",
+      usuario: "jpineda",
+      password: bcrypt.hashSync("0321200100384", 10),
+      modulo: "E",
+      isJefe: "N",
+      oficina: "SPS",
+      isActivo: "S",
+    },
+    {
+      nombre: "KAREN MILLA",
+      usuario: "kmilla",
+      password: bcrypt.hashSync("1601200200464", 10),
+      modulo: "E",
+      isJefe: "N",
+      oficina: "SPS",
+      isActivo: "S",
+    },
+
+    // DISPENSAS - SPS
+    {
+      nombre: "SARA PADILLA",
+      usuario: "spadilla",
+      password: bcrypt.hashSync("1809198100099", 10),
+      modulo: "D",
+      isJefe: "S",
+      oficina: "SPS",
+      isActivo: "S",
+    },
+    {
+      nombre: "ANA LEIVA",
+      usuario: "aleiva",
+      password: bcrypt.hashSync("1801199902477", 10),
+      modulo: "D",
+      isJefe: "N",
+      oficina: "SPS",
+      isActivo: "S",
+    },
+
+    // REGISTRO - TGU
+    {
+      nombre: "IVANNY CERRATO",
+      usuario: "imcerrato",
+      password: bcrypt.hashSync("0201199501082", 10),
+      modulo: "R",
+      isJefe: "S",
+      oficina: "TGU",
+      isActivo: "S",
+    },
+  ];
 
 
   try {
