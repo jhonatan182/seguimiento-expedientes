3f3642a77731786f2725b41df0c7b089b5d304ee
Jhonatan Vargas <jvargas@sefin.gob.hn>
2026-04-17 16:18:25 -0600

    fix: brillo imagen login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -19,7 +19,7 @@ export default function LoginPage() {
           priority
           src="/login.jpg"
           alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="absolute inset-0 h-full w-full object-cover"
         />
       </div>
     </div>
